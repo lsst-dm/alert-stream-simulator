f0617b5be1eac95767ad75fa9596dcef0d19e44c
Spencer Nelson <swnelson@uw.edu>
2020-04-16 15:30:36 -0700

    Add rubin-alert-sim command
    Add a new setup.py file which makes this project pip installable. When
    installing, install a rubin-alert-sim executable. This is a command
    line application with only one subcommand: rubin-alert-sim
    publish-file will send a file to a Kafka broker.
    
    This CLI is eventually going to house more subcommands. For now, just
    set up the scaffolding.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+install_requires = ['avro-python3', 'confluent_kafka']
+
+dev_requires = []
+
+setup(name='rubin-alert-stream-simulator',
+      version="0.1.0",
+      description="Auxiliary code for simulating rthe Vera Rubin Observatory Alert Stream",
+      url='https://github.com/lsst-dm/alert-stream-simulator',
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Development Status :: 3 - Alpha",
+          "Operating System :: POSIX :: Linux",
+      ],
+      author='Spencer Nelson',
+      author_email='swnelson@uw.edu',
+      license='GPLv3',
+      packages=['streamsim'],
+      package_dir={'': 'python'},
+      install_requires=install_requires,
+      extras_require={"dev": dev_requires},
+      scripts=["python/bin/rubin-alert-sim"],
+      zip_safe=False)

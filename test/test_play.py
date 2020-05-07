a89e0defba38d65336c8f259f8c23ca9fd2add45
Spencer Nelson <swnelson@uw.edu>
2020-05-06 19:13:03 -0700

    Remove unused imports

--- test/test_play.py
+++ test/test_play.py
@@ -19,13 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import unittest
-import os.path
-import json
-import io
 
 import pytest
 import astropy.time
-import fastavro
 
 from streamsim import creator, player, _kafka
 from test import testutils
